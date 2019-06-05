74f434487d5e1943d8a2195b48d1fa8a551ebde4
ivan <kubaice3@gmail.com>
2019-06-06 00:23:14 +0200

    Actually I'm doing sign In activity and its class

---
+++ app/src/main/java/com/example/ActivitySignIn.java
@@ -0,0 +1,11 @@
+package com.example;
+
+import android.app.Activity;
+import android.view.View;
+
+public class ActivitySignIn extends Activity {
+
+    public void forgotPassword(View view) {
+
+    }
+}
