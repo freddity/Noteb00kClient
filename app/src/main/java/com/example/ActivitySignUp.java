0a6982096143721be2460e94540d377ab5b373af
ivan <kubaice3@gmail.com>
2019-06-07 00:04:17 +0200

    activity_signup.xml done

---
+++ app/src/main/java/com/example/ActivitySignUp.java
@@ -0,0 +1,22 @@
+package com.example;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+
+public class ActivitySignUp extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+    }
+
+    public void signUpClicked(View view) {
+
+    }
+
+    public void goToSignInClicked(View view) {
+
+    }
+}
