55dbdbdb8c8e5b67bd395f2dbf81785cb991080b
ivan <kubaice3@gmail.com>
2019-06-06 20:06:43 +0200

    Load screen is done correctly

--- app/src/main/java/com/example/ActivitySplash.java
+++ app/src/main/java/com/example/ActivitySplash.java
@@ -1,18 +1,17 @@
 package com.example;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 
-public class ActivitySignIn extends Activity {
+public class ActivitySplash extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_signin);
-    }
 
-    public void forgotPassword(View view) {
-
+        Intent intent = new Intent(this, ActivitySignIn.class);
+        startActivity(intent);
+        finish();
     }
 }
