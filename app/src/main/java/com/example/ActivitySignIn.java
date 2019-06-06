af40b56ca81740bea44d64f27b4e5a56465359e4
ivan <kubaice3@gmail.com>
2019-06-06 22:55:09 +0200

    All snackbars in app/src/main/java/com/example/ActivitySignIn.java work.

--- app/src/main/java/com/example/ActivitySignIn.java
+++ app/src/main/java/com/example/ActivitySignIn.java
@@ -1,6 +1,5 @@
 package com.example;
 
-import android.app.ActionBar;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.design.widget.Snackbar;
@@ -12,29 +11,17 @@ public class ActivitySignIn extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signin);
-
-        View decorView = getWindow().getDecorView();
-        int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
-        decorView.setSystemUiVisibility(uiOptions);
-        ActionBar actionBar = getActionBar();
-        assert actionBar != null;
-        //actionBar.hide();
     }
 
     public void signInClicked(View view) {
-        //Snackbar.make(view, R.string.sign_in, Snackbar.LENGTH_SHORT).show();
-
-        Snackbar snackbar = Snackbar
-                .make(view, "www.journaldev.com", Snackbar.LENGTH_LONG);
-        snackbar.show();
+        Snackbar.make(view, R.string.sign_in, Snackbar.LENGTH_SHORT).show();
     }
 
     public void forgotPasswordClicked(View view) {
-
-
+        Snackbar.make(view, R.string.forgot_password, Snackbar.LENGTH_SHORT).show();
     }
 
     public void goToSignUpClicked(View view) {
-
+        Snackbar.make(view, R.string.have_you_account, Snackbar.LENGTH_SHORT).show();
     }
 }
