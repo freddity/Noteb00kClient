8568f79c7269d5f969fa5f6f197d4ebee8d9f6e9
ivan <kubaice3@gmail.com>
2019-06-06 20:52:08 +0200

    Load screen works.

--- app/src/main/java/com/example/ActivitySignIn.java
+++ app/src/main/java/com/example/ActivitySignIn.java
@@ -1,7 +1,9 @@
 package com.example;
 
+import android.app.ActionBar;
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.view.View;
 
 public class ActivitySignIn extends Activity {
@@ -10,9 +12,25 @@ public class ActivitySignIn extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signin);
+
+        View decorView = getWindow().getDecorView();
+        int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
+        decorView.setSystemUiVisibility(uiOptions);
+        ActionBar actionBar = getActionBar();
+        assert actionBar != null;
+        actionBar.hide();
     }
 
-    public void forgotPassword(View view) {
+    public void signInClicked(View view) {
+        //Snackbar.make(view, R.string.sign_in, Snackbar.LENGTH_SHORT).show();
+    }
+
+    public void forgotPasswordClicked(View view) {
+
+
+    }
+
+    public void goToSignUpClicked(View view) {
 
     }
 }
