3fe456f6e3147e580ed9fa06d5855b9a99449f92
ivan <kubaice3@gmail.com>
2019-06-18 23:42:36 +0200

    Now I must do this (FragmentNote.onCreateView()) using database

--- app/src/main/java/com/example/FragmentNote.java
+++ app/src/main/java/com/example/FragmentNote.java
@@ -8,6 +8,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class FragmentNote extends Fragment {
@@ -23,7 +24,7 @@ public class FragmentNote extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_notes, container, false);
 
-        RecyclerView recyclerView = view.findViewById(R.id.recyclerview);
+        /*RecyclerView recyclerView = view.findViewById(R.id.recyclerview);
 
         try {
             sqlConnector.addNote(
@@ -52,36 +53,36 @@ public class FragmentNote extends Fragment {
             e.printStackTrace();
         }
 
-        return view;
+        return view;*/
 
 
-        View view = inflater.inflate(R.layout.fragment_notes, container, false);
-
-        List<Data> data = fill_with_data();
+        List<Note> data = fill_with_data();
         RecyclerView recyclerView = (RecyclerView) view.findViewById(R.id.recyclerview);
 
         RecyclerViewAdapter adapter = new RecyclerViewAdapter(data, getContext());
         recyclerView.setAdapter(adapter);
 
-        data.add(new Data("TO JAAA", "SIEMA KURKA", R.drawable.ic_launcher));
+        data.add(new Note("TO JAAA", "SIEMA KURKA", "film"));
 
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
 
         RecyclerViewAdapter adapter2 = new RecyclerViewAdapter(data,getContext());
-        recyclerView.setAdapter(adapter);
+        recyclerView.setAdapter(adapter2);
 
         return view;
     }
 
-    /*public List<> fill_with_data() {
+    public List<Note> fill_with_data() {
 
+        ArrayList data = new ArrayList();
 
-        data.add(new Data("Batman vs Superman", "Following the destruction of Metropolis, Batman embarks on a personal vendetta against Superman ", R.drawable.ic_launcher));
-        data.add(new Data("X-Men: Apocalypse", "X-Men: Apocalypse is an upcoming American superhero film based on the X-Men characters that appear in Marvel Comics ", R.drawable.ic_launcher));
-        data.add(new Data("Captain America: Civil War", "A feud between Captain America and Iron Man leaves the Avengers in turmoil.  ", R.drawable.ic_launcher));
-        data.add(new Data("Kung Fu Panda 3", "After reuniting with his long-lost father, Po  must train a village of pandas", R.drawable.ic_launcher));
-        data.add(new Data("Warcraft", "Fleeing their dying home to colonize another, fearsome orc warriors invade the peaceful realm of Azeroth. ", R.drawable.ic_launcher));
-        data.add(new Data("Alice in Wonderland", "Alice in Wonderland: Through the Looking Glass ", R.drawable.ic_launcher));
-    }*/
+        data.add(new Note("Batman vs Superman", "Following the destruction of Metropolis, Batman embarks on a personal vendetta against Superman ", "film"));
+        data.add(new Note("X-Men: Apocalypse", "X-Men: Apocalypse is an upcoming American superhero film based on the X-Men characters that appear in Marvel Comics ", "film"));
+        data.add(new Note("Captain America: Civil War", "A feud between Captain America and Iron Man leaves the Avengers in turmoil.  ", "film"));
+        data.add(new Note("Kung Fu Panda 3", "After reuniting with his long-lost father, Po  must train a village of pandas", "film"));
+        data.add(new Note("Warcraft", "Fleeing their dying home to colonize another, fearsome orc warriors invade the peaceful realm of Azeroth. ", "film"));
+        data.add(new Note("Alice in Wonderland", "Alice in Wonderland: Through the Looking Glass ", "film"));
+        return data;
+    }
 }
 
